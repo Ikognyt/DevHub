10a536042b6aa492e847080d5a058a0e32e291f2
Filipe Andrade <lipe327@gmail.com>
2022-06-13 21:17:59 -0300

    Day 1/7 complete

--- app/src/main/java/br/com/ylotec/devhub/MainActivity.kt
+++ app/src/main/java/br/com/ylotec/devhub/MainActivity.kt
@@ -1,14 +1,23 @@
 package br.com.ylotec.devhub
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.ylotec.devhub.ui.theme.DevHubTheme
 
@@ -22,22 +31,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArtistAvatar()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun ArtistAvatar() {
+        Column {
+            Image(painter = painterResource(id = R.drawable.filipe_fb_rzd),
+                    contentDescription = "Foto do usuário")
+            Text("Filipe Andrade")
+            Text("ikognyt")
+            Text("Android Developer Jr at Venturus ")
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DevHubTheme {
-        Greeting("Android")
     }
+
 }
